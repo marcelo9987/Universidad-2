2e83aa0e6b4703c3029970075731385800b79b9b
marcelo9987 <26576174+marcelo9987@users.noreply.github.com>
2024-09-23 20:19:22 +0200

    Ejercicios 1 2 3 de la clase 3

---
+++ C2 S1 Tecnicas de Programacion Avanzada/Tema 1/Tema 1/src/Clase2/Ej1.java
@@ -0,0 +1,22 @@
+package Clase2;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Ej1
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    public static void ej1()
+    {
+        ArrayList<Integer> numeros = new ArrayList<>();
+        Scanner entrada = new 
+        for(;;)
+        {
+            System.out.print("Introduce un número: ");
+        }
+    }
+}
