ec5b8bace98317162a36e017d049db38b4fdd8eb
marcelo9987 <26576174+marcelo9987@users.noreply.github.com>
2025-06-15 20:25:58 +0200

    Limpieza

--- C2S2 Ingeniería del software/App/src/main/java/com/bicisoft/bicifast/aplicacion/MetodoPago.java
+++
@@ -1,8 +0,0 @@
-package com.bicisoft.bicifast.aplicacion;
-
-/**
- * Clase que representa el tipo de método de pago.
- */
-public enum MetodoPago {
-    TARJETA, EFECTIVO, NO_DEFINIDO
-}
--- C2S2 Ingeniería del software/App/src/main/java/com/bicisoft/bicifast/gui/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Paquete que contiene la interfaz gráfica de usuario (GUI) de la aplicación BiciFast.
- * <p>
- * Incluye ventanas principales, diálogos, paneles y componentes visuales para la interacción con el usuario.
- * La GUI actúa como "vista" y notifica los eventos a los controladores.
- * <p>
- * Autoría: Equipo Bicisoft, 2025
- */
-package com.bicisoft.bicifast.gui;
