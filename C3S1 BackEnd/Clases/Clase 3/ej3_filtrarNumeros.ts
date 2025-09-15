8c91058178adadfa36fcefbd635e5a9f8a961fb4
marcelo <imarcelo8000@gmail.com>
2025-09-15 16:26:37 +0200

    C3S1 Backend:
    ej3: terminado

--- C3S1 BackEnd/Clases/Clase 3/ej3_filtrarNumeros.ts
+++ C3S1 BackEnd/Clases/Clase 3/ej3_filtrarNumeros.ts
@@ -1,4 +1,9 @@
-const arrayDeNumeros:Array<number> =[2,5,10,66,50,4,78,16,5,21,15,99]
+const arrayDeNumeros: Array<number> = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 66, 50, 4, 78, 16, 21, 15, 99]
 
-const multiplos_2_5:Array<number> = arrayDeNumeros.filter((x)=>(((x%2==0) && (x%5==0))))
+const multiplos_2_5: Array<number> = arrayDeNumeros.filter((x) =>
+    {
+        return ((x % 2 == 0) && (x % 5 == 0))
+    }
+)
+
 console.log(multiplos_2_5);
