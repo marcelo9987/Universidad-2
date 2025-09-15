cb22a0d801012c3335e73a9af907c6aeb388fd28
marcelo <imarcelo8000@gmail.com>
2025-09-15 16:09:43 +0200

    C3S1 Backend:
    ej2: Mejora con las funciones de ej1_crearTipo

--- C3S1 BackEnd/Clases/Clase 3/ej2_buscarMarcaPrenda.ts
+++ C3S1 BackEnd/Clases/Clase 3/ej2_buscarMarcaPrenda.ts
@@ -11,6 +11,12 @@ type Prenda =
         , marca: Marca
     };
 
+
+const buscarMarca = (prenda:Prenda):Marca=>
+{
+    return prenda.marca
+};
+
 const marca1:Marca = {id:1,nombre:"A"};
 const marca2:Marca = {id:2,nombre:"B"};
 const marca3:Marca = {id:3,nombre:"C"};
@@ -19,8 +25,9 @@ const marca4:Marca = {id:4,nombre:"D"};
 const prenda1:Prenda = {id:1,nombre:"prenda1", marca:marca2};
 const prenda2:Prenda = {id:3,nombre:"prenda2", marca:marca3};
 const prenda3:Prenda = {id:4,nombre:"prenda3", marca:marca3};
-const prenda4:Prenda = {id:5,nombre:"prenda3", marca:marca4};
+const prenda4:Prenda = {id:5,nombre:"prenda4", marca:marca4};
+const prenda5:Prenda = {id:5,nombre:"prenda5", marca:marca1};
 
-const arrayDePrendas:Array<Prenda> = [prenda1, prenda2, prenda3, prenda4]
-const arrayDeMarcas:Array<Marca> = arrayDePrendas.map((x)=>x.marca)
+const arrayDePrendas:Array<Prenda> = [prenda1, prenda2, prenda3, prenda4, prenda5]
+const arrayDeMarcas:Array<Marca> = arrayDePrendas.map((x)=>buscarMarca(x))
 console.log(arrayDeMarcas);
