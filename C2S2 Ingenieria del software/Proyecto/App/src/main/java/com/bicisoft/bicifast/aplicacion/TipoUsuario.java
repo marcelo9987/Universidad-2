feda47f0ef72ba4a5e807f0f4dae5b7f9a5b9c38
Marcelo9987 <imarcelo8000@gmail.com>
2025-12-11 20:16:54 +0100

    C2 S2: Ing software Reorganización

--- C2S2 Ingenieria del software/Proyecto/App/src/main/java/com/bicisoft/bicifast/aplicacion/TipoUsuario.java
+++
@@ -1,9 +0,0 @@
-package com.bicisoft.bicifast.aplicacion;
-
-/**
- * Enumeración que representa los tipos de usuario en la aplicación.
- */
-public enum TipoUsuario {
-    Admin, Mant, NORMAL, NO_DEFINIDO
-
-}
--- C2S2 Ingenieria del software/Proyecto/App/src/main/java/com/bicisoft/bicifast/misc/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Paquete que contiene clases de utilidad para a aplicación BiciFast.
- * <p>
- * Incluye utilidades generales como criptografía e internacionalización.
- * Las clases de este paquete son reutilizables y no dependen de ninguna capa específica del sistema.
- * <p>
- * Autoría: Equipo Bicisoft, 2025
- */
-package com.bicisoft.bicifast.misc;
